9fb3aaa3544762c7f144693b941e5900a4fef1a9
Duccaa <gojicduca@yahoo.com>
2020-09-25 11:40:49 +0200

    Updates

---
+++ precache-manifest.feb96206f76ced5242c54b4a7d2511f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "149d930b5336288da917065ab4088ac6",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "a78384f7beb863464025",
+    "url": "/movie-app/static/css/main.8245b68e.chunk.css"
+  },
+  {
+    "revision": "587e2db6a580cc207d81",
+    "url": "/movie-app/static/js/2.e1845f35.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/movie-app/static/js/2.e1845f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a78384f7beb863464025",
+    "url": "/movie-app/static/js/main.9105e569.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
